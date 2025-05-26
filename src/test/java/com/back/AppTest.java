7610dd4f1d02b0c1911d5cc9e3866512b0264e30
jiseopshin <wltjq1203@icloud.com>
2025-05-26 15:45:05 +0900

    테스트를 실행을 위해 입출력 리다이렉트를 실시하는 클래스 작성

--- src/test/java/com/back/AppTest.java
+++ src/test/java/com/back/AppTest.java
@@ -4,18 +4,24 @@ package com.back;
 import com.back.standard.util.TestUtil;
 import com.back.wiseSaying.WiseSayingApp;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 public class AppTest {
-
     public static String run(String input) {
-        Scanner scanner = TestUtil.genScanner(input.stripIndent().trim());
-        new WiseSayingApp(scanner).start();
-        String string = scanner.nextLine();
+        Scanner scanner = TestUtil.genScanner(input);
 
-        return string;
+        ByteArrayOutputStream capturedOutput = TestUtil.setOutToByteArray();
+
+        WiseSayingApp wiseSayingApp = new WiseSayingApp(scanner, "./src/test/resources");
+        wiseSayingApp.start();
+
+        String outputString = capturedOutput.toString();
+
+        TestUtil.clearSetOutToByteArray(capturedOutput);
+
+        return outputString;
     }
-
     public static void clear() {
     }
 }
