36f9eecd087b09af98b42f40d5c175d6fb097741
jiseopshin <wltjq1203@icloud.com>
2025-05-26 17:44:37 +0900

    12단계

--- src/test/java/com/back/AppTest.java
+++ src/test/java/com/back/AppTest.java
@@ -5,15 +5,22 @@ import com.back.standard.util.TestUtil;
 import com.back.wiseSaying.WiseSayingApp;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class AppTest {
+    static String appRoot = "./src/test/resources";
+
     public static String run(String input) {
         Scanner scanner = TestUtil.genScanner(input);
 
         ByteArrayOutputStream capturedOutput = TestUtil.setOutToByteArray();
 
-        WiseSayingApp wiseSayingApp = new WiseSayingApp(scanner, "./src/test/resources");
+        WiseSayingApp wiseSayingApp = new WiseSayingApp(scanner, appRoot);
         wiseSayingApp.start();
 
         String outputString = capturedOutput.toString();
@@ -22,6 +29,29 @@ public class AppTest {
 
         return outputString;
     }
+
     public static void clear() {
+        deleteAll();
     }
+
+    public static void deleteAll() {
+        Path directory = Paths.get(appRoot + "/db/wiseSaying");
+
+        try {
+            if (Files.exists(directory)) {
+                Files.walk(directory)
+                        .sorted(Comparator.reverseOrder())
+                        .forEach(path -> {
+                            try {
+                                Files.delete(path);
+                            } catch (IOException e) {
+                                throw new RuntimeException("Failed to delete: " + path, e);
+                            }
+                        });
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
